7c274887b63a9fafd5ff7814e5d77296421c1281
Venkateshbandla509 <bandlavenkatesh509@gmail.com>
2023-11-10 10:30:15 +0530

    first commit

---
+++ Google_search.py
@@ -0,0 +1,9 @@
+#we need the googlesearch module if not exist for git
+#use " pip install googlesearch-python "
+#import thr search from googlesearch module
+from googlesearch import search
+
+query = input("Enter the word or sentence you need to search in google : ")
+
+for i in search(query, num_results= 5, lang="en" , proxy=None):
+    print(i)
